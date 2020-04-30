fd2cd5ffd0e7cf6d3d938bf6a9d98013b9a2b27c
reid harward <reid.harward@gmail.com>
2020-04-30 19:11:59 -0400

    added topics, listing of all files, some other mods

--- server.js
+++ server.js
@@ -13,10 +13,12 @@ app.use(express.static('public/data',cors()));
 app.use(express.static('http://127.0.0.1:7000/'+ '/public',cors()));
 app.use(express.json({limit: '10mb'}));
 const database = new Datastore('database.db');
-//const wordStore = new Datastore('wordStore.db');
+const topiclist = new Datastore('topiclist.db');
+topiclist.loadDatabase();
 database.loadDatabase();
 var http = require('http');
 var fs = require('graceful-fs');
+
 app.get('/getsubtitles', (request,response) => {
     database.find({},(err,data)=>{
       if (err){
@@ -29,6 +31,8 @@ app.get('/getsubtitles', (request,response) => {
       //console.log('Count = '+ database.count );})
     });
 });
+
+
 app.post ('/getsubtitles', (request1,response) =>  {
 let Yserial = "";
  Yserial = request1.body.Yserial;
@@ -45,4 +49,63 @@ getSubtitles({
    console.log('in here');
 });
 })
+//below is the function to remove the database totally.  
+app.post('/dropdatabase', (request1, response) => {
+console.log('DROPPING DATABASE')
+  database.remove({ }, { multi: true }, function (err, numRemoved) {
+    database.loadDatabase(function (err) {
+      // done
+    });
+  });
+})
 
+app.post('/droprecord', (request1, response) => {
+  
+  let currentRecord = "";
+  currentRecord = (request1.body.data);
+  console.log('currentRecord ='+currentRecord);
+  database.remove({ _id: currentRecord }, {}, function (err, numRemoved){
+
+  console.log('DROPPED RECORD');
+})
+database.persistence.compactDatafile();
+  });
+
+app.post('/addtopic', (request1,response) =>{
+    let currentRecord = "";
+    currentRecord = request1.body.Eserial;
+    console.log('current Record = '+ currentRecord);
+    topiclist.insert({currentRecord}, function (err, currentRecord){
+      console.log('ADDED RECORD');
+      let data = JSON.stringify(currentRecord);
+      response.send(data);
+      topiclist.persistence.compactDatafile()
+     })
+     //return (null, currentRecord);
+    
+  })
+
+  app.get('/populatetopic', (request,response) => {
+  console.log('inside the populate topic function');
+    let currentRecord = "";
+    //currentRecord = request.body.data;
+    //console.log('currentRecord ='+ currentRecord);
+   // database.remove({ _id: currentRecord }, {}, function (err, numRemoved){
+    //console.log('POPULATING THE TOPIC DROP DOWN');
+    
+      topiclist.find({},(err,data)=>{
+        if (err){
+          response.end();
+          return;
+        }else{
+      response.send(data);
+        console.log('DATA = '+ data)
+        }//response = data;
+       //database.count({}, function (err, count) {
+        //response.end();
+        //console.log('Count = '+ database.count );})
+      });
+  });
+
+ // })
+   // });
