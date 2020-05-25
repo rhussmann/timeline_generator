8795bf582e93baa8ca9523f091a61c788d9fcc53
reid harward <reid.harward@gmail.com>
2020-05-25 08:23:17 -0400

    pushed on Monday,  May 25 at 8:18 - changed the nedb declaration from var to const

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ var data = 'vIJdypOqlL4';
 function onYouTubeIframeAPIReady(data) {
     let dataInner = "";
     dataInner = data;
-    console.log('I changed this Sunday May 24, at 5:25pm');
+    console.log('I changed this Monday May 25, at 8:22am');
   //  console.log('dataInner = '+dataInner);
     player = new YT.Player('video-placeholder', {
         width: 600,
