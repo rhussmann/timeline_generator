b192a5f16449b3cc5f4b63ee93c705b63c2f92ae
reid harward <reid.harward@gmail.com>
2020-05-28 21:25:04 -0400

    pushed on Thu May 28 at 9:23pm put app.listen below app.use

--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ var data = 'vIJdypOqlL4';
 function onYouTubeIframeAPIReady(data) {
     let dataInner = "";
     dataInner = data;
-    console.log('I changed this Thursday. May 27, at 12:08am');
+    console.log('I changed this Thursday. May 27, at 9:24pm');
   //  console.log('dataInner = '+dataInner);
     player = new YT.Player('video-placeholder', {
         width: 600,
